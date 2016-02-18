0412620c0f5178ef11ea7febf9f8bf45fdf87971
afabijan <aleksander.fabijan@me.com>
2016-02-18 23:28:40 +0100

    added headers

--- server.js
+++ server.js
@@ -53,14 +53,12 @@ var Person     = require('./models/person');
 var Itinerary = require('./models/itinerary');
 
 
-
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
- });
-
-
+});
 
 
 // ROUTES FOR OUR API
